21446184f55a954e18e7bd62a7bc0932b2fe136b
Shubham Kumar <shubhamkumar7549yuv@gmail.com>
2026-02-26 12:37:09 +0530

    stockdetail

--- frontend/src/pages/StockDetailsPage.jsx
+++ frontend/src/pages/StockDetailsPage.jsx
@@ -4,11 +4,13 @@ import API from "../services/api";
 
 const StockDetailsPage = () => {
   const { symbol } = useParams();
-  const [data, setData] = useState(null);
+  const [data, setData] = useState({});
 
   useEffect(() => {
-    API.get(`/stocks/${symbol}`).then((res) =>
-      setData(res.data["Global Quote"])
+    API.get(`/stocks/${symbol}`).then((res) => {
+      setData(res.data)
+    console.log(res.data);
+    }
     );
   }, [symbol]);
 
@@ -17,9 +19,9 @@ const StockDetailsPage = () => {
   return (
     <div>
       <h2>{symbol}</h2>
-      <p>Price: {data["05. price"]}</p>
-      <p>High: {data["03. high"]}</p>
-      <p>Low: {data["04. low"]}</p>
+      <p>Price: {data.close}</p>
+      <p>High: {data.high}</p>
+      <p>Low: {data.low}</p>
     </div>
   );
 };
